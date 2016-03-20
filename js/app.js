e480a43883cf770338409990ddb81d3bd45a0a3d
mradambeck <mr.adambeck@gmail.com>
2016-03-20 09:24:15 -0700

    some bug fixes, tweaks and changes.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,19 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
   console.log("sanity check! Javascript is running.");
-  // all code to manipulate the DOM
-  // goes inside this function
   //establish board
-// 0.1 VARIABLES
   var boardArray = [[null,null,null],
                     [null,null,null],
                     [null,null,null]];
   var playerTurn = 'X';
   var turnsPlayed = 0;
   var winner = null;
-
-// 0.2 FUNCTIONS
+  var lastPlayed = null;
+  var gameOver = false;
 
   function showBoardArray(){
     console.log(boardArray);
   }
-
-// 0.3 JQUERY STUFFS
 
   $(".btn-danger").on("click", function resetGame(){
     boardArray = [[null,null,null],
@@ -27,58 +22,111 @@ $(document).ready(function() {
     playerTurn = 'X';
     turnsPlayed = 0;
     winner = null;
+    gameOver = false;
     $(".box").text("");
     $('.x').removeClass("x", 100000, "linear");
     $('.o').removeClass("o", 100000, "linear");
-    console.log(boardArray);
+    $("body").removeClass("cats-game", 1000);
+
+    $("#mainTitle").text(lastPlayed + " won the last round.");
+    showBoardArray();
     console.log("playerTurn: " + playerTurn + " turnsPlayed: " + turnsPlayed + " Winner: " + winner);
   });
 
   $(".box").on("click", function handleClick(event){
-    // Logs play to boardArray
-    var cellID = $(this).attr('id');
-      console.log("cell ID is: " + cellID);
-    // * A user should not be able to claim a box that has already been claimed for `X` or `O`.
-    for (var i=0; i<cellID.length; i++) {
-      cellIDArray = cellID.split("");
-      cellIDArray[i] = parseInt(cellIDArray[i], 10);
-    }
-      // Requirement: Users should be able to click on any empty box to make a move.
-    if ( boardArray[cellIDArray[0]][cellIDArray[1]] === null) { // Checks to see if a move has been made in that cell.
-      // handles CSS to visualize gameplay
-        // Requirement: Every move should alternate between marking an `X` and `O` (the two players).
-      if(playerTurn === 'X'){
-        $(this).text("X");
-        boardArray[cellIDArray[0]][cellIDArray[1]] = playerTurn; // Logs it into the array, like a boss!  Woot, woot!
-        playerTurn = 'O'; // switches to next player
-        $(this).addClass("x"); // changes bg color
-        turnsPlayed++;
-      } else {
-        $(this).text("O");
-        boardArray[cellIDArray[0]][cellIDArray[1]] = playerTurn; // Logs it into the array, like a boss!  Woot, woot!
-        playerTurn = 'X'; // switches to next player
-        $(this).addClass("o"); // changes bg color
-        turnsPlayed++;
+    // Grabs css ID, converts to integer, logs to boardArray
+    if (!gameOver){
+      var cellID = $(this).attr('id');
+        console.log("cell ID is: " + cellID);
+
+      for (var i=0; i<cellID.length; i++) {
+        cellIDArray = cellID.split("");
+        cellIDArray[i] = parseInt(cellIDArray[i], 10);
       }
+      if ( boardArray[cellIDArray[0]][cellIDArray[1]] === null) { // Checks to see if a move has been made in that cell.
+        // handles CSS to visualize gameplay
+        if(playerTurn === 'X'){
+          $(this).text("X");
+          boardArray[cellIDArray[0]][cellIDArray[1]] = playerTurn; // Logs it into the array, like a boss!  Woot, woot!
+          playerTurn = 'O'; // switches to next player
+          lastPlayed = 'X'; // allows us to keep track of last player (to mark the winner)
+          $(this).addClass("x"); // changes bg color
+          turnsPlayed++;
+        } else {
+          $(this).text("O");
+          boardArray[cellIDArray[0]][cellIDArray[1]] = playerTurn;
+          playerTurn = 'X';
+          lastPlayed = 'O';
+          $(this).addClass("o");
+          turnsPlayed++;
+        }
 
-    } else {
-      console.log("Player " + playerTurn + " tried playing on " + boardArray[cellIDArray[0]][cellIDArray[1]] + "but it was already taken.");
+        // determines winner
+        if (((boardArray[0][0]==='X')&&(boardArray[0][1]==='X')&&(boardArray[0][2]==='X')) ||
+          ((boardArray[0][0]==='O')&&(boardArray[0][1]==='O')&&(boardArray[0][2]==='O'))) {
+            winner = lastPlayed;
+            console.log(winner + " wins!");
+            $("#mainTitle").text(winner + " WINS!");
+            gameOver = true;
+        } else if (((boardArray[1][0]==='X')&&(boardArray[1][1]==='X')&&(boardArray[1][2]==='X')) ||
+          ((boardArray[1][0]==='O')&&(boardArray[1][1]==='O')&&(boardArray[1][2]==='O'))){
+            winner = lastPlayed;
+            console.log(winner + " wins!");
+            $("#mainTitle").text(winner + " WINS!");
+            gameOver = true;
+        } else if (((boardArray[2][0]==='X')&&(boardArray[2][1]==='X')&&(boardArray[2][2]==='X')) ||
+          ((boardArray[2][0]==='O')&&(boardArray[2][1]==='O')&&(boardArray[2][2]==='O'))){
+            winner = lastPlayed;
+            console.log(winner + " wins!");
+            $("#mainTitle").text(winner + " WINS!");
+            gameOver = true;
+        } else if (((boardArray[0][0]==='X')&&(boardArray[1][0]==='X')&&(boardArray[2][0]==='X')) ||
+          ((boardArray[0][0]==='O')&&(boardArray[1][0]==='O')&&(boardArray[2][0]==='O'))){
+            winner = lastPlayed;
+            console.log(winner + " wins!");
+            $("#mainTitle").text(winner + " WINS!");
+            gameOver = true;
+        } else if (((boardArray[0][1]==='X')&&(boardArray[1][1]==='X')&&(boardArray[2][1]==='X')) ||
+          ((boardArray[0][1]==='O')&&(boardArray[1][1]==='O')&&(boardArray[2][1]==='O'))){
+            winner = lastPlayed;
+            console.log(winner + " wins!");
+            $("#mainTitle").text(winner + " WINS!");
+            gameOver = true;
+        } else if (((boardArray[0][2]==='X')&&(boardArray[1][2]==='X')&&(boardArray[2][2]==='X')) ||
+          ((boardArray[0][2]==='O')&&(boardArray[1][2]==='O')&&(boardArray[2][2]==='O'))){
+            winner = lastPlayed;
+            console.log(winner + " wins!");
+            $("#mainTitle").text(winner + " WINS!");
+            gameOver = true;
+        } else if (((boardArray[0][2]==='X')&&(boardArray[1][2]==='X')&&(boardArray[2][2]==='X')) ||
+          ((boardArray[0][2]==='O')&&(boardArray[1][2]==='O')&&(boardArray[2][2]==='O'))){
+            winner = lastPlayed;
+            console.log(winner + " wins!");
+            $("#mainTitle").text(winner + " WINS!");
+            gameOver = true;
+        } else if (((boardArray[0][0]==='X')&&(boardArray[1][1]==='X')&&(boardArray[2][2]==='X')) ||
+          ((boardArray[0][0]==='O')&&(boardArray[1][1]==='O')&&(boardArray[2][2]==='O'))){
+            winner = lastPlayed;
+            console.log(winner + " wins!");
+            $("#mainTitle").text(winner + " WINS!");
+            gameOver = true;
+        } else if (((boardArray[0][2]==='X')&&(boardArray[1][1]==='X')&&(boardArray[2][0]==='X')) ||
+          ((boardArray[0][2]==='O')&&(boardArray[1][1]==='O')&&(boardArray[2][0]==='O'))){
+            winner = lastPlayed;
+            console.log(winner + " wins!");
+            $("#mainTitle").text(winner + " WINS!");
+            gameOver = true;
+        } else if ((turnsPlayed === 9)&& !winner){
+            console.log("Cat's Game!");
+            $("#mainTitle").text("Cat's Game!");
+            $('body').addClass("cats-game");
+            gameOver = true;
+        }
+      } else {
+        console.log("Player " + playerTurn + " tried playing on " + boardArray[cellIDArray[0]][cellIDArray[1]] + " but it was already taken.");
+      }
     }
-
-    // determines winner
-    if (turnsPlayed === 9){
-      $("#mainTitle").text("CAT'S GAME!");
-      $('body').addClass("cats-game");
-        console.log("CAT'S GAME!");
-    } else {
-        console.log("keep playing");
-    }
-
     // Logs boardArray for debugging
-    console.log(boardArray);
+    showBoardArray();
   });
 });
-
-// if ($(this).hasClass('x' || 'o')) {
-// DON'T LET THEM MAKE A MARK
-// }
--- winning-combos.js
+++ winning-combos.js
@@ -1,53 +1,41 @@
-boardArray = [[true,true,true],
-              [null,null,null],
-              [null,null,null]];
+// boardArray = [[true,true,true],
+//               [null,null,null],
+//               [null,null,null]];
+//
+// boardArray[0][0]===boardArray[0][1]===boardArray[0][2];
+//
+// boardArray = [[null,null,null],
+//               [true,true,true],
+//               [null,null,null]];
+//
+// boardArray[1][0]===boardArray[1][1]===boardArray[1][2];
 
-boardArray[0][0]===boardArray[0][1]===boardArray[0][2];
+// boardArray = [[null,null,null],
+//               [null,null,null],
+//               [true,true,true]];
+// (boardArray[2][0]==='X')&&(boardArray[2][1]==='X')&&(boardArray[2][2]==='X')
 
-boardArray = [[null,null,null],
-              [true,true,true],
-              [null,null,null]];
+// boardArray = [[true,null,null],
+//               [true,null,null],
+//               [true,null,null]];
+// (boardArray[0][0]==='X')&&(boardArray[1][0]==='X')&&(boardArray[2][0]==='X')
+//
+// boardArray = [[null,true,null],
+//               [null,true,null],
+//               [null,true,null]];
+// (boardArray[0][1]==='X')&&(boardArray[1][1]==='X')&&(boardArray[2][1]==='X')
 
-boardArray[1][0]===boardArray[1][1]===boardArray[1][2];
-
-boardArray = [[null,null,null],
-              [null,null,null],
-              [true,true,true]];
-
-boardArray[2][0]===boardArray[2][1]===boardArray[2][2];
-
-boardArray = [[true,null,null],
-              [true,null,null],
-              [true,null,null]];
-
-boardArray[0][0]===boardArray[1][0]===boardArray[2][0];
-
-boardArray = [[null,true,null],
-              [null,true,null],
-              [null,true,null]];
-
-boardArray[0][1]===boardArray[1][1]===boardArray[2][1];
-
-boardArray = [[null,null,true],
-              [null,null,true],
-              [null,null,true]];
-
-boardArray[0][2]===boardArray[1][2]===boardArray[2][2];
-
-boardArray = [[true,null,null],
-              [null,true,null],
-              [null,null,true]];
-
-boardArray[0][0]===boardArray[1][1]===boardArray[2][2];
-
-boardArray = [[null,null,true],
-              [null,true,null],
-              [true,null,null]];
-
-boardArray[0][2]===boardArray[1][1]===boardArray[2][0];
-
-
-
-
-
-              // * Users should be able to click a "reset" button to clear all `X`'s and `O`'s from the board and restart the game.
+// boardArray = [[null,null,true],
+//               [null,null,true],
+//               [null,null,true]];
+// (boardArray[0][2]==='X')&&(boardArray[1][2]==='X')&&(boardArray[2][2]==='X')
+//
+// boardArray = [[true,null,null],
+//               [null,true,null],
+//               [null,null,true]];
+// (boardArray[0][0]==='X')&&(boardArray[1][1]==='X')&&(boardArray[2][2]==='X')
+//
+// boardArray = [[null,null,true],
+//               [null,true,null],
+//               [true,null,null]];
+// (boardArray[0][2]==='X')&&(boardArray[1][1]==='X')&&(boardArray[2][0]==='X')
